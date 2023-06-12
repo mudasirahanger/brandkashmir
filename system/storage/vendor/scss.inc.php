a114ee84a837a075721c4bf819ca60abd3d32fe7
mudasirahanger <mudasir.ahanger@digitalindia.gov.in>
2023-06-12 13:45:06 +0530

    v1 is ready

--- system/storage/vendor/scss.inc.php
+++ system/storage/vendor/scss.inc.php
@@ -1750,7 +1750,8 @@ class scssc {
 
 		foreach ($args as $arg) {
 			list($key, $value) = $arg;
-			$key = $key[1];
+			//$key = $key[1];
+			if (is_array($key)) $key = $key[1];
 			if (empty($key)) {
 				$posArgs[] = $value;
 			} else {
